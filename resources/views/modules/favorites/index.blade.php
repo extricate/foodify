7bbc30eb5c3a97501623f1287bcbaa3fb76376d1
Herman <hsfnelissen@gmail.com>
2018-05-14 21:44:44 +0200

    Removed unnecessary titles from views.

--- resources/views/modules/favorites/index.blade.php
+++ resources/views/modules/favorites/index.blade.php
@@ -8,11 +8,6 @@
 
 @section('content')
     <div class="row">
-        <div class="col-8">
-            <h1 class="primary">Your favorites</h1>
-        </div>
-    </div>
-    <div class="row">
         <div class="col-12 card-columns">
             @foreach($favorites as $favorite)
                     @include('modules.recipes.partials.recipe-card', ['recipe' => $favorite->recipe])
