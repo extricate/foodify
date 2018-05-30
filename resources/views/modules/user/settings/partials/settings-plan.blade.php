374fad62a51b3e0bccf7b513f9b2508723733402
Herman <hsfnelissen@gmail.com>
2018-05-30 10:40:03 +0200

    Replaced dedicated forms to one dynamic radio button form.

--- resources/views/modules/user/settings/partials/settings-plan.blade.php
+++ resources/views/modules/user/settings/partials/settings-plan.blade.php
@@ -3,9 +3,9 @@
                 'route' => ['settings.patch'],
                 'enctype' => 'multipart/form-data'
             ]) !!}
-@include('modules.user.settings.partials.plan.show_plan_in_navbar')
-@include('modules.user.settings.partials.plan.show_planning_options_on_recipes_index')
-@include('modules.user.settings.partials.plan.show_plan_on_recipes_index')
-@include('modules.user.settings.partials.plan.show_plan_on_dashboard')
+@include('modules.user.settings.partials.radio', ['setting' => 'show_plan_in_navbar'])
+@include('modules.user.settings.partials.radio', ['setting' => 'show_planning_options_on_recipes_index'])
+@include('modules.user.settings.partials.radio', ['setting' => 'show_plan_on_recipe_index'])
+@include('modules.user.settings.partials.radio', ['setting' => 'show_plan_on_dashboard'])
 <button type="submit" class="btn btn-primary">Change settings</button>
 {!! Form::close() !!}
