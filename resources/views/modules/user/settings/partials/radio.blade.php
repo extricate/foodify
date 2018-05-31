9be2b4512f528d36a3703eb090e425739bb0860b
Herman <hsfnelissen@gmail.com>
2018-05-31 17:19:37 +0200

    Made settings responsive.

--- resources/views/modules/user/settings/partials/radio.blade.php
+++ resources/views/modules/user/settings/partials/radio.blade.php
@@ -3,31 +3,29 @@
         <div class="col-md-6 align-self-center">
             <strong>{{ str_replace('_', ' ', ucfirst($setting)) }}:</strong>
         </div>
-        <div class="col-md-6 d-inline-flex align-items-center">
-            <div class="d-inline-flex float-right">
-                <div class="mdc-form-field">
-                    <div class="mdc-radio">
-                        <input class="mdc-radio__native-control" type="radio" id="{{ $setting }}-1"
-                               name="{{ $setting }}"
-                               @if(auth()->user()->getSetting($setting)) checked @endif value="1">
+        <div class="col-md-6">
+            <div class="mdc-form-field d-flex float-right">
+                <div class="mdc-radio">
+                    <input class="mdc-radio__native-control" type="radio" id="{{ $setting }}-1"
+                           name="{{ $setting }}"
+                           @if(auth()->user()->getSetting($setting)) checked @endif value="1">
 
-                        <div class="mdc-radio__background">
-                            <div class="mdc-radio__outer-circle"></div>
-                            <div class="mdc-radio__inner-circle"></div>
-                        </div>
+                    <div class="mdc-radio__background">
+                        <div class="mdc-radio__outer-circle"></div>
+                        <div class="mdc-radio__inner-circle"></div>
                     </div>
-                    <label for="{{ $setting }}-1" class="mb-0">Show</label>
-                    <div class="mdc-radio">
-                        <input class="mdc-radio__native-control" type="radio" id="{{ $setting }}-2"
-                               name="{{ $setting }}"
-                               @if(!auth()->user()->getSetting($setting))) checked @endif value="0">
-                        <div class="mdc-radio__background">
-                            <div class="mdc-radio__outer-circle"></div>
-                            <div class="mdc-radio__inner-circle"></div>
-                        </div>
-                    </div>
-                    <label for="{{ $setting }}-2" class="mb-0">Hide</label>
                 </div>
+                <label for="{{ $setting }}-1" class="mb-0">Show</label>
+                <div class="mdc-radio">
+                    <input class="mdc-radio__native-control" type="radio" id="{{ $setting }}-2"
+                           name="{{ $setting }}"
+                           @if(!auth()->user()->getSetting($setting))) checked @endif value="0">
+                    <div class="mdc-radio__background">
+                        <div class="mdc-radio__outer-circle"></div>
+                        <div class="mdc-radio__inner-circle"></div>
+                    </div>
+                </div>
+                <label for="{{ $setting }}-2" class="mb-0">Hide</label>
             </div>
         </div>
     </div>
